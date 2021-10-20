9ed0e9092fbf78ed757e0116d4bf952ca9d9e8af
guojian <424139777@qq.com>
2021-10-20 18:40:20 +0800

    tijiao

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,41 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    return prisma.user.create({
+      data: {
+        name: 'Alice',
+        email: 'alice@prisma.io',
+        posts: {
+          create: { title: 'Hello World' },
+        },
+        profile: {
+          create: { bio: 'I like turtles' },
+        },
+      },
+    });
+  }
+  @Get('test')
+  getHelloTest() {
+    return prisma.user.findMany({
+      include: {
+        posts: true,
+        profile: true,
+      },
+    });
+  }
+  @Get('testdelete')
+  async getHelloTestDelete() {
+    return await prisma.user.delete({
+      where: {
+        id: 2,
+      },
+    });
   }
 }
